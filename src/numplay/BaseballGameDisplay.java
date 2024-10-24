02c98dda839311f89f549c13bd659978e6929b5b
baksihwan <baksh999@naver.com>
2024-10-24 16:49:44 +0900

    LV1 baseballgameDisplay 구현

--- src/numplay/BaseballGameDisplay.java
+++ src/numplay/BaseballGameDisplay.java
@@ -1,6 +1,6 @@
 package numplay;
 
-public class BaseballGameDisplay {
+public class BaseballGameDisplay {   //baseball 경기 수행 구현
     public static void displayHint(int strike, int ball) {
         if( strike == 0 && ball == 0){
             System.out.println("아웃");
