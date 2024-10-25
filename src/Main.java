18572c948438b67b5f5124540039d5b82fb4e27a
baksihwan <baksh999@naver.com>
2024-10-25 14:15:49 +0900

    LV1 실행화면 구현

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ import numplay.BaseballGame;
 
 public class Main {
     public static void main(String[] args) {
-        // 게임실행창
+        // 게임실행창 구현
         System.out.println("(✿˵･ ૩( ･ิᴗ･ิ)♡<숫자 야구 게임에 오신 것을 환영합니다>(✿˵･ ૩( ･ิᴗ･ิ)♡");
         System.out.println("~~게임을 시작하겠습니다~~");
         BaseballGame game = new BaseballGame();
